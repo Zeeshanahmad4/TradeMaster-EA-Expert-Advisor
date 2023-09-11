7d8584e310338c7bc1ad6cc66363a3e26e640ab6
Zeeshan Ahmad <ahmadzee26@gmail.com>
2023-09-11 13:43:57 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -1,1 +1,7 @@
-# Configuration settings (lot size, TP, SL, etc.) 
+# config/settings.py
+
+LOT_SIZE = 1.0  # default lot size
+TP = 10  # Take Profit default value
+SL = 10  # Stop Loss default value
+PRICE_CHANGE_THRESHOLD = 1.0  # Price change threshold for marking points
+
