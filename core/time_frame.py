81cfd9bf1815cb313f0b211e7292065f6c2e35fd
Zeeshan Ahmad <ahmadzee26@gmail.com>
2023-09-11 13:45:25 +0500

    Update time_frame.py

--- core/time_frame.py
+++ core/time_frame.py
@@ -1,1 +1,12 @@
-# Logic for changing and handling time frames 
+# core/time_frame.py
+
+CURRENT_TIME_FRAME = "M15"  # default time frame
+
+def switch_time_frame(new_time_frame):
+    """
+    Switch to a new time frame for analysis.
+    :param new_time_frame: The time frame to switch to.
+    :return: None
+    """
+    global CURRENT_TIME_FRAME
+    CURRENT_TIME_FRAME = new_time_frame
