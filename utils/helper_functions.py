fa06e3a2554ea58ec99be5d590836e4a9d14db7b
Zeeshan Ahmad <ahmadzee26@gmail.com>
2023-09-11 13:47:57 +0500

    Update helper_functions.py

--- utils/helper_functions.py
+++ utils/helper_functions.py
@@ -1,1 +1,7 @@
-# Additional helper functions and utilities 
+# utils/helper_functions.py
+
+def calculate_average(trading_data):
+    """
+    Calculate the average of the trading data.
+    """
+    return sum(trading_data) / len(trading_data) if trading_data else 0
