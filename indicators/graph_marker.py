d637debc50ac6a484f9569f5cbba335c6de6f401
Zeeshan Ahmad <ahmadzee26@gmail.com>
2023-09-11 13:47:04 +0500

    Update graph_marker.py

--- indicators/graph_marker.py
+++ indicators/graph_marker.py
@@ -1,1 +1,15 @@
-# Code for the indicator to mark on the graph 
+# indicators/graph_marker.py
+
+def mark_graph(trading_data):
+    """
+    Analyze the trading data and mark points of interest on the graph.
+    :param trading_data: The trading data to be analyzed.
+    :return: List of points (coordinates) to mark on the graph.
+    """
+    marked_points = []
+    for i in range(1, len(trading_data)):
+        if trading_data[i] - trading_data[i-1] > 1:  # Significant increase
+            marked_points.append((i, trading_data[i]))
+        elif trading_data[i-1] - trading_data[i] > 1:  # Significant decrease
+            marked_points.append((i, trading_data[i]))
+    return marked_points
