12056f065a9cede28ac3e6edba710aca407490b6
Zeeshan Ahmad <ahmadzee26@gmail.com>
2023-09-11 13:46:36 +0500

    Update lot_size.py

--- core/lot_size.py
+++ core/lot_size.py
@@ -1,1 +1,11 @@
-# Logic for adjusting the lot size 
+# core/lot_size.py
+
+from config import settings
+
+def adjust_lot_size(new_lot_size):
+    """
+    Adjust the lot size for trading.
+    :param new_lot_size: The new lot size to adjust to.
+    :return: None
+    """
+    settings.LOT_SIZE = new_lot_size
